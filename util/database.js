7509a3aa025c19eb77eb92f89189377a72700fb9
dennis <denizponce16@gmail.com>
2022-03-08 23:05:03 -0500

    added procfile

--- util/database.js
+++ util/database.js
@@ -11,8 +11,21 @@ const sequelize = new Sequelize({
   port: 5432,
   database: process.env.DB, // example
   dialect: 'postgres',
-  logging: false
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 });
+
+//Host ec2-44-195-191-252.compute-1.amazonaws.com
+// Database d22obp5k24peir
+// User hzwpwzqmbxcynu
+// Password 61a8d1dab3d79859977a275534a69fbc4ee9337b31d3a64a4cc7c673ef1eb2d5
+// URI postgres://hzwpwzqmbxcynu:61a8d1dab3d79859977a275534a69fbc4ee9337b31d3a64a4cc7c673ef1eb2d5@ec2-44-195-191-252.compute-1.amazonaws.com:5432/d22obp5k24peir
+// Heroku CLI heroku pg:psql postgresql-adjacent-87985 --app sequelize-app-hrk
 
 // Connect to Heroku cloud
 // const sequelize = new Sequelize({
